24019726296af3438f7b4920a0263f4facc08ff0
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-02-27 20:09:18 +0000

    Loading the data from the file

--- FinalDirectory.rb
+++ FinalDirectory.rb
@@ -1,5 +1,6 @@
 @students = []
 
+
 def input_students
   puts "Please enter the names of the students."
   puts "To finish, just hit return twice."
@@ -31,6 +32,7 @@ def print_menu
   puts "1. Input the students."
   puts "2. Show the students."
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit."
 end
 
@@ -41,11 +43,20 @@ def show_students
 end
 
 def save_students
-  file = File.open("students.csv", "w")
+  file = File.open(".gitignore/students.csv", "w")
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
     file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  file = File.open(".gitignore/students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
 end
@@ -58,6 +69,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit
   else
